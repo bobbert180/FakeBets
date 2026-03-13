2d83ff6dc5cfe3d806028e2c2c1c10d80f19d4f5
bobbert180 <bobomalley1001@gmail.com>
2026-03-13 16:52:25 -0500

    Create new users page

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // Only send one response per request
+  res.render('users', { title: 'Users', users: ['Alice', 'Bob', 'Charlie'] });
 });
 
 module.exports = router;
